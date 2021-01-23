111c0ffba794d4dfd95797e7ac6457a63979be98
stayscary <stayscary@mail.ru>
2021-01-23 15:02:43 +0300

    Homework_2

---
+++ src/Homework_2.java
@@ -0,0 +1,18 @@
+public class Homework_2 {
+    public static void main(String[] args) {
+        // Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел.
+        // Присвойте ей произвольное дробное значение.
+        // Типом данной переменной является - Double.
+        double doubleNum;
+        doubleNum = 3.141592653589793;
+
+        // Создайте переменную целочисленного типа с наименьшим диапазоном чисел.
+        // Присвойте  ей наибольшее возможное значение.
+        // Типом данной переменной является - Float.
+        float floatNum;
+        floatNum = Float.MAX_VALUE;
+
+        System.out.println("Double number = " + doubleNum);
+        System.out.println("Float number = " + floatNum);
+    }
+}
