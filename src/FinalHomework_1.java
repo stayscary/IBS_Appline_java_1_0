6fec8c7ffa17bec7995d7e3a69583f3381207065
stayscary <stayscary@mail.ru>
2021-02-03 22:04:59 +0300

    FinalHomework_1, refactoring

--- src/FinalHomework_1.java
+++ src/FinalHomework_1.java
@@ -7,10 +7,13 @@ public class FinalHomework_1 {
         // Пользователь вводит текущий курс и количество рублей.
         // Итоговое значение должно быть округлено до двух знаков после запятой.
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Курс доллара: ");
         double dollar = scanner.nextDouble();
+
         System.out.print("Количество рублей: ");
         double rub = scanner.nextDouble();
+
         System.out.printf("Итого: %.2f долларов", (rub / dollar));
     }
 }
